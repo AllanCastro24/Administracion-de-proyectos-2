e670a67d50b846faada1ab8a6d86efdec65e37a8
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2021-11-25 17:47:43 -0700

    Se agregó home en backend

--- backend/home.php
+++ backend/home.php
@@ -4,13 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home </title>
-    <link rel="stylesheet" href="css/login.css">
+    <title>Inicio </title>
+    <link rel="stylesheet" href="css/home.css">
 </head>
 
 <body>
-    
-    <a> Bienvenido </a>
+    <div class="titulo">
+		<h5>Administrador de base de datos para Diccionario - Traductor</h5>
+	</div>
+
+    <div class="menu">
+		<h5>Navegación</h5>
+		<ul>
+			<li><a href="#"> Inicio</a></li>
+			<li><a href="#"> Diccionario</a></li>
+			<li><a href="#"> Dialectos</a></li>
+			<li><a href="#">Usuarios</a></li>
+		</ul>
+	</div>
 
     <footer>Traductor - Diccionario 2021 (Administrador de base de datos)</footer><br><br><br>
 </body>
